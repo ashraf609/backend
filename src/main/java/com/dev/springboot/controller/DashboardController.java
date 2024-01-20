eee69cf12216e0dd8b97d02e709ce7ba0b151d7b
ashraf609 <ettqftqfi2222@gmail.com>
2024-01-20 05:19:06 +0100

    uppopo

--- src/main/java/com/dev/springboot/controller/DashboardController.java
+++ src/main/java/com/dev/springboot/controller/DashboardController.java
@@ -16,17 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.dev.springboot.exception.ArticleNotFoundException;
 import com.dev.springboot.model.Article;
 import com.dev.springboot.service.IArticleService;
 @Controller
+@RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
 
